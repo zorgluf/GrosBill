5b04843e6f7072b56a72559142663c6f04c178bb
zorgluf <zorgluf@gmail.com>
2025-08-05 19:09:20 +0200

    fix config for docker

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ WARN = 30
 ERROR = 40
 DISABLED = 50
 
-LOGDIR = "app/logs"
-RESULTSPATH = 'app/viz/results.csv'
-TMPMODELDIR = "app/zoo/tmp"
-MODELDIR = "app/zoo"
+LOGDIR = "logs"
+RESULTSPATH = 'viz/results.csv'
+TMPMODELDIR = "zoo/tmp"
+MODELDIR = "zoo"
