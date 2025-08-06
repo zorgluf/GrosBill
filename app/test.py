b2f097c3dd5aafe5138d65c985e5dd76ce8ba8ec
zorgluf <zorgluf@gmail.com>
2025-08-06 21:35:50 +0200

    make test.py compatible with empty steps

--- app/test.py
+++ app/test.py
@@ -16,119 +16,81 @@ import numpy as np
 
 
 def main(args):
-
-  logger = configure(config.LOGDIR)
-
-  render_mode = args.render_mode
-
-  if args.seed == 0:
-    seed = random.randint(0,1000)
-  else:
-    seed = args.seed
-
-  if args.debug:
-    logger.set_level(config.DEBUG)
-    logging.basicConfig(level=logging.DEBUG)
-    render_mode = 'human'
-  else:
-    logger.set_level(config.INFO)
-    
-  #make environment
-  env = get_environment(args.env_name)(verbose = args.verbose, manual = args.manual, render_mode = render_mode)
-  set_random_seed(seed)
-
-  total_rewards = {}
-
-  if args.recommend:
-    ppo_model = load_model(env, 'best_model.zip')
-    ppo_agent = Agent('best_model', ppo_model)
-  else:
-    ppo_agent = None
-
-
-  agents = []
-
-  #load the agents
-  if len(args.agents) != env.n_players:
-    raise Exception(f'{len(args.agents)} players specified but this is a {env.n_players} player game!')
-
-  for i, agent in enumerate(args.agents):
-    if agent == 'human':
-      agent_obj = Agent('human')
-    elif agent == 'rules':
-      agent_obj = Agent('rules')
-    elif agent == 'base':
-      base_model = load_model(env, 'base.zip', args.device)
-      agent_obj = Agent('base', base_model)   
+    # Set up logging
+    logger = logging.getLogger("test.py")
+    if args.debug:
+        logger.setLevel(config.DEBUG)
     else:
-      ppo_model = load_model(env, f'{agent}.zip', args.device)
-      agent_obj = Agent(agent, ppo_model)
-    agents.append(agent_obj)
-    total_rewards[agent_obj.id] = 0
-  
-  #play games
-  logger.info(f'\nPlaying {args.games} games...')
-  for game in range(args.games):
-    players = agents[:]
+        logger.setLevel(config.INFO)
 
-    if args.randomise_players:
-      random.shuffle(players)
+    # Set up seed
+    if args.seed == 0:
+        seed = random.randint(0,1000)
+    else:
+        seed = args.seed
 
-    obs = env.reset(seed = seed)
-    done = False
-    
-    for i, p in enumerate(players):
-      logger.debug(f'Player {i+1} = {p.name}')
+    #make environment
+    env = get_environment(args.env_name)()
+    set_random_seed(seed)
 
-    while not done:
+    total_rewards = {}
 
-      current_player = players[env.current_player_num]
-      env.render()
-      logger.debug(f'\nCurrent player name: {current_player.name}')
+    agents = []
 
-      if args.recommend and current_player.name in ['human', 'rules']:
-        # show recommendation from last loaded model
-        logger.debug(f'\nRecommendation by {ppo_agent.name}:')
-        action = ppo_agent.choose_action(env, choose_best_action = True, mask_invalid_actions = True)
+    #load the agents
+    if len(args.agents) != env.n_players:
+        raise Exception(f'{len(args.agents)} players specified but this is a {env.n_players} player game!')
 
-      if current_player.name == 'human':
-        if hasattr(env,"handle_interactive_action") and env.handle_interactive_action == True:
-          action = env.get_interactive_action()
+    for i, agent in enumerate(args.agents):
+        if agent == 'base':
+            base_model = load_model(env, 'base.zip', args.device)
+            agent_obj = Agent('base', base_model)   
         else:
-          action = input('\nPlease choose an action: ')
-          try:
-            # for int actions
-            action = int(action)
-          except:
-            # for MulitDiscrete action input as list TODO
-            action = eval(action)
-      elif current_player.name == 'rules':
-        logger.debug(f'\n{current_player.name} model choices')
-        action = current_player.choose_action(env, choose_best_action = False, mask_invalid_actions = True)
-      else:
-        logger.debug(f'\n{current_player.name} model choices')
-        action = current_player.choose_action(env, choose_best_action = args.best, mask_invalid_actions = True)
+            ppo_model = load_model(env, f'{agent}.zip', args.device)
+            agent_obj = Agent(agent, ppo_model)
+        agents.append(agent_obj)
+        total_rewards[agent_obj.id] = 0
+  
+    #play games
+    logger.info(f'Playing {args.games} games...')
+    for game in range(args.games):
+        players = agents[:]
 
-      obs, reward, done, _ , _ = env.step(action)
+        if args.randomise_players:
+            random.shuffle(players)
 
-      for r, player in zip(reward, players):
-        total_rewards[player.id] += r
-        player.points += r
-
-      if args.cont:
-        input('Press any key to continue')
+        obs = env.reset(seed = seed)
+        done = False
     
-    env.render()
+        for i, p in enumerate(players):
+            logger.debug(f'Player {i+1} = {p.name}')
 
-    logger.info(f"Played {game + 1} games: {total_rewards}")
+        while not done:
 
-    if args.write_results:
-      write_results(players, game, args.games, env.turns_taken)
+            current_player = players[env.current_player]
 
-    for p in players:
-      p.points = 0
+            action = current_player.choose_action(env, choose_best_action = args.best, mask_invalid_actions = True)
+            logger.debug(f'Current player name: {current_player.name}, choosing action: {action}')
 
-  env.close()
+            obs, reward, done, _ , info = env.step(action)
+            while info['next_step_no_action'] and not done:
+                obs, reward, done, _ , info = env.step(-1)
+                logger.debug(f'No action needed, continuing...')
+
+            for r, player in zip(reward, players):
+                total_rewards[player.id] += r
+                player.points += r
+
+
+        logger.info(f"Played {game + 1} games: {total_rewards}")
+
+        if args.write_results:
+            write_results(players, game, args.games, env.turns_taken)
+
+        for p in players:
+            p.points = 0
+
+        env.close()
 
 
 def cli() -> None:
@@ -146,20 +108,12 @@ def cli() -> None:
                 , help="Make AI agents choose the best move (rather than sampling)")
   parser.add_argument("--games", "-g", type = int, default = 1
                 , help="Number of games to play)")
-  parser.add_argument("--render_mode", "-rm", type = str, default = "human_web"
-                 , help="Render mode to use in game gym env")
   parser.add_argument("--debug", "-d",  action = 'store_true', default = False
             , help="Show logs to debug level")
   parser.add_argument("--verbose", "-v",  action = 'store_true', default = False
             , help="Show observation on debug logging")
-  parser.add_argument("--manual", "-m",  action = 'store_true', default = False
-            , help="Manual update of the game state on step")
   parser.add_argument("--randomise_players", "-r",  action = 'store_true', default = False
             , help="Randomise the player order")
-  parser.add_argument("--recommend", "-re",  action = 'store_true', default = False
-            , help="Make recommendations on humans turns")
-  parser.add_argument("--cont", "-c",  action = 'store_true', default = False
-            , help="Pause after each turn to wait for user to continue")
   parser.add_argument("--env_name", "-e",  type = str, default = 'TicTacToe'
             , help="Which game to play?")
   parser.add_argument("--write_results", "-w",  action = 'store_true', default = False
